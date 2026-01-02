615591998d97a74861aa8806601e892e2e0eb8ef
Amey <72331282+vazurkar@users.noreply.github.com>
2026-01-02 17:59:49 +0530

    question solved for reversse string

--- Array/besttimetobuyandsellstock.java
+++ Array/besttimetobuyandsellstock.java
@@ -2,7 +2,7 @@ package Array;
 
 public class besttimetobuyandsellstock {
     public static void main(String[] args) {
-        int prices[] = {7,6,4,3,1};
+        int prices[] = {7,1,5,3,6,4};
         int buyprice = prices[0];
         int profit = 0;
         for(int i=1;i<prices.length;i++){
